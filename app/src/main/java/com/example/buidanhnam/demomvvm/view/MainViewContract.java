eea492a5b75c3b30be3a7fe59605cd041417448b
NamBD <bui.danh.nam@framgia.com>
2017-12-28 13:33:39 +0700

    create struct MVVM and init Data Model

---
+++ app/src/main/java/com/example/buidanhnam/demomvvm/view/MainViewContract.java
@@ -0,0 +1,18 @@
+package com.example.buidanhnam.demomvvm.view;
+
+import com.example.buidanhnam.demomvvm.model.People;
+
+import java.util.List;
+
+/**
+ * Created by Bui Danh Nam on 28/12/2017.
+ */
+
+public interface MainViewContract {
+    interface MainView {
+        public void loadData(List<People> peopleList) ;
+    }
+    interface MainModelView{
+        public void destroy();
+    }
+}
